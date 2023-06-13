72724cbb6e015e7eaeb041e39b6e8ab1f4677b8a
tombortniak <tombortniak@gmail.com>
2023-06-13 23:08:38 +0200

    Add artwork model

---
+++ db/migrate/20230613210737_create_artworks.rb
@@ -0,0 +1,15 @@
+class CreateArtworks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :artworks do |t|
+      t.string :title
+      t.string :image
+      t.references :artist, null: false, foreign_key: true
+      t.string :location
+      t.string :city
+      t.string :country
+      t.string :continent
+
+      t.timestamps
+    end
+  end
+end

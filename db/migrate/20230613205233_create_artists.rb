bc2db99d7767f2e13e58942ebfee0a2a4bc2f507
tombortniak <tombortniak@gmail.com>
2023-06-17 00:14:30 +0200

    Fix migrations syntax

--- db/migrate/20230613205233_create_artists.rb
+++ db/migrate/20230613205233_create_artists.rb
@@ -1,9 +1,9 @@
 class CreateArtists < ActiveRecord::Migration[7.0]
   def change
     create_table :artists do |t|
-      t.string :name, :null: false
-      t.string :country, :null: false
-      t.string :period, :null: false
+      t.string :name, null: false
+      t.string :country, null: false
+      t.string :period, null: false
     end
   end
 end
--- db/migrate/20230613210737_create_artworks.rb
+++ db/migrate/20230613210737_create_artworks.rb
@@ -1,7 +1,7 @@
 class CreateArtworks < ActiveRecord::Migration[7.0]
   def change
     create_table :artworks do |t|
-      t.string :title, :null: false
+      t.string :title, null: false
       t.string :image
       t.references :artist, null: false, foreign_key: true
       t.string :location, null: false

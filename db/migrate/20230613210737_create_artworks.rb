8eb37a4e005e17f2b679dc86c35051afd3bc8a0f
tombortniak <tombortniak@gmail.com>
2023-06-16 23:54:55 +0200

    Add not null constraint to particular model fields

--- db/migrate/20230613210737_create_artworks.rb
+++ db/migrate/20230613210737_create_artworks.rb
@@ -1,13 +1,13 @@
 class CreateArtworks < ActiveRecord::Migration[7.0]
   def change
     create_table :artworks do |t|
-      t.string :title
+      t.string :title, :null: false
       t.string :image
       t.references :artist, null: false, foreign_key: true
-      t.string :location
-      t.string :city
-      t.string :country
-      t.string :continent
+      t.string :location, null: false
+      t.string :city, null: false
+      t.string :country, null: false
+      t.string :continent, null: false
     end
   end
 end
